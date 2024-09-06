b4e0dcfe3e044d5b81cd580923aa025aeabfc7a6
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2024-09-06 19:17:15 -0300

    logica para UI do jogo

--- index.js
+++ index.js
@@ -21,59 +21,88 @@ let playerPoints = 0;
 let computerPoints = 0;
 let empate = 0;
 
-
 function round(playerSelection, computerSelection) {
     let resultado;
 
     if (playerSelection == computerSelection) {
         resultado = arrayAnswers[0];
         empate++;
+        currentScores.innerText = `Pontos do jogador: ${playerPoints} e Pontos do computador: ${computerPoints}.`
         return resultado;
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         resultado = arrayAnswers[1];
         playerPoints++;
+        currentScores.innerText = `Pontos do jogador: ${playerPoints} e Pontos do computador: ${computerPoints}.`
         return resultado;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         resultado = arrayAnswers[2];
         playerPoints++;
+        currentScores.innerText = `Pontos do jogador: ${playerPoints} e Pontos do computador: ${computerPoints}.`
         return resultado;
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         resultado = arrayAnswers[3];
         computerPoints++
+        currentScores.innerText = `Pontos do jogador: ${playerPoints} e Pontos do computador: ${computerPoints}.`
         return resultado;
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         resultado = arrayAnswers[4];
         playerPoints++;
+        currentScores.innerText = `Pontos do jogador: ${playerPoints} e Pontos do computador: ${computerPoints}.`
         return resultado;
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         resultado = arrayAnswers[5];
         computerPoints++;
+        currentScores.innerText = `Pontos do jogador: ${playerPoints} e Pontos do computador: ${computerPoints}.`
         return resultado;
     } else {
         resultado = arrayAnswers[6];
         computerPoints++;
+        currentScores.innerText = `Pontos do jogador: ${playerPoints} e Pontos do computador: ${computerPoints}.`
         return resultado;
     }
 }
 
-function game() {
+//Event handler to buttons
 
-    let counter = 0;
+let playerSelection;
+let computerChoise;
+let roundWinner;
+let counter = 0;
 
-    while (counter < 5 + empate) {
-        let playerSelection = prompt("Digite sua Opção").toLowerCase();
-        let computerChoise = getComputerChoice();
-        let roundWinner = round(playerSelection, computerChoise);
-        counter++;
-        console.log("escolha do PLAYER: " + playerSelection);
-        console.log("escolha do COMPUTADOR: " + computerChoise);
-        console.log("quem ganhou o ROUND: " + roundWinner)
+const container = document.querySelector(".container");
+const roundResult = document.createElement("p");
+const currentScores = document.createElement("p");
+
+
+let button = document.querySelector("#playerChoises");
+button.addEventListener('click', (event) => {
+    counter++;
+    let target = event.target;
+
+    if (target.id === 'paper') {
+        playerSelection = "paper";
+        computerChoise = getComputerChoice();
+        return roundResult.innerText = round(playerSelection, computerChoise);
     }
-    // console.log("valor do counter:" + counter);
-    // console.log("valor do player " + playerPoints);
+    else if (target.id === 'rock') {
+        playerSelection = "rock";
+        computerChoise = getComputerChoice();
+        return roundResult.innerText = round(playerSelection, computerChoise);
+    }
+    else {
+        playerSelection = "scissors";
+        computerChoise = getComputerChoice();
+        return roundResult.innerText = round(playerSelection, computerChoise);
+    }
+});
 
+function game() {
     return playerPoints > computerPoints ? "Jogador venceu!" : "Computador venceu."
 
+    console.log("escolha do PLAYER: " + playerSelection);
+    console.log("escolha do COMPUTADOR: " + computerChoise);
+    console.log("quem ganhou o ROUND: " + roundWinner);
 }
 
-console.log("quem ganhou O GAME: " + game());
+container.appendChild(roundResult);
+container.appendChild(currentScores);
